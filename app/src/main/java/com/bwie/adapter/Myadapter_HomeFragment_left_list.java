860bf844b51631d959a73a92261d24480dfda6af
chenwenmeng <1289196829@qq.com>
2017-02-09 11:53:35 +0800

    月光茶人

---
+++ app/src/main/java/com/bwie/adapter/Myadapter_HomeFragment_left_list.java
@@ -0,0 +1,46 @@
+package com.bwie.adapter;
+
+import android.view.View;
+
+
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+
+
+import com.bwie.bean.Bean;
+
+
+import java.util.List;
+
+/**
+ * 类描述:
+ * 作者：陈文梦
+ * 时间:2017/2/9 11:21
+ * 邮箱:18310832074@163.com
+ */
+
+public class Myadapter_HomeFragment_left_list extends BaseAdapter {
+
+    private List<Bean.DataBean.CategoriesBean> categoriesBeanList;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
